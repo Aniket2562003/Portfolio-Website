c96e703fffb6d821e5a4464f70a48b687a4e4d60
Aniket Rawat <aniketrawat2450@gmail.com>
2024-01-26 20:17:26 +0530

    Contact info page complete, Section Profile complete

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  icon.classList.toggle("open");
-  menu.classList.toggle("open");
+function openContactForm() {
+  window.location.href = "ContactForm.html";
 }
-//
